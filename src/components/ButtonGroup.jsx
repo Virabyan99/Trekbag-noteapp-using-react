7f4ebfdf7192508e567a99567f6368529a481129
Andranik Virabyan <gmparstone99@gmail.com>
2024-08-16 03:09:12 -0800

    First commit

---
+++ src/components/ButtonGroup.jsx
@@ -0,0 +1,17 @@
+import { secondaryButtons } from '../lib/constants'
+import Button from './Button'
+
+
+export default function ButtonGroup() {
+  return (
+    <section className="button-group">
+      {secondaryButtons.map((text) => {
+        return (
+          <Button key={text} type="secondary">
+            {text}
+          </Button>
+        )
+      })}
+    </section>
+  )
+}
---
+++ src/lib/constants.js
@@ -0,0 +1,26 @@
+export const initialItems = [
+  {
+    id: 1,
+    name: 'Good Mood',
+    packed: true,
+  },
+  {
+    id: 2,
+    name: 'Passport',
+    packed: false,
+  },
+  {
+    id: 3,
+    name: 'Phone charger',
+    packed: false,
+  },
+]
+
+export const secondaryButtons = [
+  'MArk all as Completed',
+  'Mark all as incomplete',
+  'Reset to initial',
+  'Remove All items',
+]
+
+
